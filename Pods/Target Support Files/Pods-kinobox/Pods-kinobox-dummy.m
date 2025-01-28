1e918fdb868e97006638373b5039f2017a91c0e0
denis <bluesky171@mail.ru>
2025-01-28 18:56:13 +0300

    add Snapkit and Alamofire

---
+++ Pods/Target Support Files/Pods-kinobox/Pods-kinobox-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_kinobox : NSObject
+@end
+@implementation PodsDummy_Pods_kinobox
+@end
--- kinobox/ViewController.swift
+++ kinobox/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
 
 class ViewController: UIViewController {
 
