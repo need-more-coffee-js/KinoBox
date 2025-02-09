8b171dd9432ce3c9a883345c5b92a5b8d265a0cf
denis <bluesky171@mail.ru>
2025-02-09 23:20:45 +0300

    add new function for clean table data

--- kinobox/ViewController.swift
+++ kinobox/ViewController.swift
@@ -11,18 +11,22 @@ import Alamofire
 import Kingfisher
 
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource , UISearchTextFieldDelegate{
     
     let userTextField = UITextField()
     let stackView = UIStackView()
     let searchButton = UIButton()
     let topFilmButton = UIButton()
+    let clearButton = UIButton()
     let resultView = UITableView()
     var resultKeywordTopFilm: [FilmElement] = []
+    
+    var isSubscribedToTextFieldChanges = false // флаг для отслеживания подписки
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        userTextField.delegate = self
         addElementsOnView()
         configure()
         constraintsConfigure()
@@ -31,10 +35,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         resultView.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
         searchButton.addTarget(self, action: #selector(searchByKeyword), for: .touchUpInside)
         topFilmButton.addTarget(self, action: #selector(searchTopFilm), for: .touchUpInside)
+        clearButton.addTarget(self, action: #selector(clearTable), for: .touchUpInside)
     }
-    
+//MARK: - UI Configure
     func addElementsOnView(){
         view.addSubview(userTextField)
+        view.addSubview(clearButton)
         view.addSubview(stackView)
         view.addSubview(resultView)
     }
@@ -44,7 +50,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         userTextField.layer.cornerRadius = 10
         userTextField.layer.borderWidth = 1
-        userTextField.clearButtonMode = .whileEditing
+        userTextField.clearButtonMode = .always
+        userTextField.placeholder = " Введите название фильма"
+        
+        clearButton.isHidden = true
+        clearButton.backgroundColor = .systemRed
+        clearButton.layer.cornerRadius = 10
+        clearButton.setImage(UIImage(systemName: "trash"), for: .normal)
+        
         
         searchButton.backgroundColor = .systemBlue
         searchButton.setTitle("Поиск", for: .normal)
@@ -67,11 +80,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func constraintsConfigure(){
         userTextField.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin)
-            make.centerX.equalTo(view)
-            make.left.right.equalToSuperview().inset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
             make.height.equalTo(40)
         }
-        
+
         stackView.snp.makeConstraints { make in
             make.top.equalTo(userTextField.snp.bottom).offset(20)
             make.left.right.equalToSuperview().inset(35)
@@ -84,7 +96,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         topFilmButton.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.left.right.equalToSuperview().inset(20)
+            make.leading.equalToSuperview().inset(20)
+            make.trailing.equalTo(clearButton.snp.leading).offset(-5)
+        }
+        clearButton.snp.makeConstraints { make in
+            make.centerY.equalTo(topFilmButton)
+            make.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+            make.width.equalTo(50)
         }
         
         resultView.snp.makeConstraints { make in
@@ -93,7 +112,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             make.left.right.equalToSuperview().inset(20)
         }
     }
-    
+//MARK: - Clear function for topFilmButton
+    @objc func clearTable(){
+        clearButton.isHidden = true
+        resultKeywordTopFilm.removeAll()
+        resultView.reloadData()
+    }
+//MARK: - Function search by keyword
      @objc func searchByKeyword(){
          if (userTextField.text?.isEmpty)!{
               alert(title: "Пустой запрос", message: "Пожалуйства,введите название фильма")
@@ -123,9 +148,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
         task.resume()
+        subscribeToTextFieldChanges()
     }
-    
+//MARK: - Function search top films
     @objc func searchTopFilm(){
+       clearButton.isHidden = false
+
        let userData = URL(string:Api.urlTopFilm)
        var request = URLRequest(url: userData!)
        
@@ -151,7 +179,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
        }
        task.resume()
    }
+//MARK: - Getting notified when UITextField begin editing
+    func subscribeToTextFieldChanges() {
+        if !isSubscribedToTextFieldChanges {
+            userTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+            isSubscribedToTextFieldChanges = true
+        }
+    }
 
+    func unsubscribeFromTextFieldChanges() {
+        if isSubscribedToTextFieldChanges {
+            userTextField.removeTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+            isSubscribedToTextFieldChanges = false
+            print("Подписка на изменения текста отключена")
+        }
+    }
+    
+    @objc func textFieldDidChange() {
+        if userTextField.text?.isEmpty ?? true {
+            clearTableData()
+        }
+    }
+//MARK: - CLEAR TABLE DATA
+    func clearTableData() {
+        resultKeywordTopFilm.removeAll()
+        resultView.reloadData()
+
+        unsubscribeFromTextFieldChanges()
+    }
+//MARK: - Table view logic
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         resultKeywordTopFilm.count
     }
@@ -193,21 +249,29 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                             filmDetailVC.originalTitle = filmByID.nameOriginal
                             filmDetailVC.posterURL = filmByID.posterURL
                             filmDetailVC.descriptionText = filmByID.description
-                            if filmByID.ratingKinopoisk == nil {
-                                filmDetailVC.ratingsText = "no info"
-                            }else {
-                                filmDetailVC.ratingsText = String(describing: filmByID.ratingKinopoisk!)
-                            }
-                            if filmByID.year == nil {
-                                filmDetailVC.yearText = "no info"
-                            }else {
-                                filmDetailVC.yearText = String(describing: filmByID.year!)
-                            }
-                            if filmByID.filmLength == nil {
-                                filmDetailVC.durationText = "no info"
-                            }else {
-                                filmDetailVC.durationText = String(describing: filmByID.filmLength!)
-                            }
+                            filmDetailVC.ratingsText = {
+                                if let rating = filmByID.ratingKinopoisk {
+                                    return String(describing: rating)
+                                } else {
+                                    return "no info"
+                                }
+                            }()
+
+                            filmDetailVC.yearText = {
+                                if let year = filmByID.year {
+                                    return String(describing: year)
+                                } else {
+                                    return "no info"
+                                }
+                            }()
+
+                            filmDetailVC.durationText = {
+                                if let duration = filmByID.filmLength {
+                                    return String(describing: duration)
+                                } else {
+                                    return "no info"
+                                }
+                            }()
                             print(filmByID)
                             self.resultView.reloadData()
                             self.present(filmDetailVC, animated: true, completion: nil)
